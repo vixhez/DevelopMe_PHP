d0775ccb88dac6f02c5734109a1d78f31dd1935e
Victoria Herod <vicky_herod@live.com>
2020-11-03 14:17:59 +0000

    03-01 complete

---
+++ 03-regex/01-hasNumber.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function hasNumber($str) {
+    return preg_match("/[0-9]+/", $str) === 1;
+};
+
+dump(hasNumber("blah")); // false
+dump(hasNumber("bl3h")); // true
+dump(hasNumber("bl3h5")); // true
